59fca53a350bf3e353781487206fa7968e0d3dc6
Phil Isaac <isaac.phil@gmail.com>
2013-11-22 16:41:53 -0500

    Network connectivity working

--- TheBeerProject/src/com/example/thebeerproject/MainActivity.java
+++ TheBeerProject/src/com/example/thebeerproject/MainActivity.java
@@ -1,25 +1,41 @@
 package com.example.thebeerproject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.thebeerproject.MESSAGE";
+	private static final String DEBUG_TAG = "HttpExample";
+	private EditText editText;
+	private TextView textView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		editText = (EditText) findViewById(R.id.edit_message);
+		textView = (TextView) findViewById(R.id.myText);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,22 +67,87 @@ public class MainActivity extends Activity {
 	// First check Internet connection and navigate user to settings if it's not available
 	// Otherwise get message contents and put in an intent -> open new activity
 	public void sendMessage(View view) {
-		
-		ConnectivityManager connMgr = (ConnectivityManager) 
-		        getSystemService(Context.CONNECTIVITY_SERVICE);
-		    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-		    if (networkInfo != null && networkInfo.isConnected()) {
-		        // fetch data from LCBOapi, open new activity with intent containing message string
-		    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-				EditText editText = (EditText) findViewById(R.id.edit_message);
-				String message = editText.getText().toString();
-				intent.putExtra(EXTRA_MESSAGE, message);
-				startActivity(intent);
-		    } else {
-		        // open settings, allow user to connect
-		    	DialogFragment networkDialog = new NetworkConnectDialogFragment();
-		    	networkDialog.show(getFragmentManager(), "network");
-//		    	startActivity(new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
+		String stringURL = editText.getText().toString();
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+		    // fetch data from LCBOapi, open new activity with intent containing message string
+			new DownloadWebpageTask().execute("http://lcboapi.com/products/18");
+//		    Intent intent = new Intent(this, DisplayMessageActivity.class);
+//			EditText editText = (EditText) findViewById(R.id.edit_message);
+//			String message = editText.getText().toString();
+//			intent.putExtra(EXTRA_MESSAGE, message);
+//			startActivity(intent);
+		} else {
+			textView.setText("No network connection acailable.");
+		    // open settings, allow user to connect
+		    //DialogFragment networkDialog = new NetworkConnectDialogFragment();
+		    //networkDialog.show(getFragmentManager(), "network");
+//		    startActivity(new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
 		    }
+	}
+	
+	// Uses AsyncTask to create a task away from the main UI thread. This task takes a 
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+       @Override
+       protected String doInBackground(String... urls) {
+             
+           // params comes from the execute() call: params[0] is the url.
+           try {
+               return downloadUrl(urls[0]);
+           } catch (IOException e) {
+               return "Unable to retrieve web page. URL may be invalid.";
+           }
+       }
+       // onPostExecute displays the results of the AsyncTask.
+       @Override
+       protected void onPostExecute(String result) {
+           textView.setText(result);
+      }
+   }
+ // Given a URL, establishes an HttpUrlConnection and retrieves
+ // the web page content as a InputStream, which it returns as
+ // a string.
+	private String downloadUrl(String myurl) throws IOException {
+	    InputStream is = null;
+	    // Only display the first 500 characters of the retrieved
+	    // web page content.
+	    int len = 500;
+	        
+	    try {
+	        URL url = new URL(myurl);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setReadTimeout(10000 /* milliseconds */);
+	        conn.setConnectTimeout(15000 /* milliseconds */);
+	        conn.setRequestMethod("GET");
+	        conn.setDoInput(true);
+	        // Starts the query
+	        conn.connect();
+	        int response = conn.getResponseCode();
+	        Log.d(DEBUG_TAG, "The response is: " + response);
+	        is = conn.getInputStream();
+		         // Convert the InputStream into a string
+	        String contentAsString = readIt(is, len);
+	        return contentAsString;
+	        
+	    // Makes sure that the InputStream is closed after the app is
+	    // finished using it.
+	    } finally {
+	        if (is != null) {
+	            is.close();
+	        } 
+	    }
+	}
+	// Reads an InputStream and converts it to a String.
+	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+	    Reader reader = null;
+	    reader = new InputStreamReader(stream, "UTF-8");        
+	    char[] buffer = new char[len];
+	    reader.read(buffer);
+	    return new String(buffer);
 	}
 }
